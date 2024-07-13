c5e324f990f7658ff4cdb7877399aa83b9598800
rohits-web03 <rohit.dey302003@gmail.com>
2024-07-14 03:55:22 +0530

    Initial commit. Also added tailwindcss.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+    import "../global.css";
+  </script>
+<div class="bg-neutral-900 absolute inset-0 text-white mx-auto">
+    <slot />
+</div> 
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="flex flex-col justify-center items-center gap-4 h-full">
+    <h1 class="text-4xl text-blue-500">Welcome to Coding Phase of TSOC'24</h1>
+    <p class="text-2xl">Visit <a class="no-underline text-teal-500" href="https://tsoc.dev/">tsoc.dev</a> to know more</p>
+</div>
+
